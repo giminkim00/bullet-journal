90d2dcedf9705238bf678775126f3f47015c95eb
giminkim <kgm0218@gmail.com>
2026-09-02 15:26:48 +0900

    feat: MemberRepository, MemberService

---
+++ src/test/java/com/example/bulletjournal/service/MemberServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.bulletjournal.service;
+
+import com.example.bulletjournal.domain.Member;
+import com.example.bulletjournal.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long savedId = memberService.join(member);
+
+
+        //then
+        Assertions.assertThat(member).isEqualTo(memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //Given
+        Member member1 = new Member();
+        member1.setName("kim");
+        Member member2 = new Member();
+        member2.setName("kim");
+        //When
+        memberService.join(member1);
+        //Then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+}
